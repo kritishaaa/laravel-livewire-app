169d149ee1b16f4f824b2cb9f1c2804e9b5dd3b0
kritisha <kritishamoktan530@gmail.com>
2024-08-28 20:25:08 +0545

    added code for homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\ShowHome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ShowHome::class)->name('home');
