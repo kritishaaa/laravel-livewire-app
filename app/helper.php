c5a7068a2125ae4e328ef573007d60a665307d69
kritisha <kritishamoktan530@gmail.com>
2024-09-12 09:59:31 +0545

    removed log

--- app/helper.php
+++ app/helper.php
@@ -2,7 +2,6 @@
 use App\Models\Service;
 function getServices(){
     $services = Service::orderBy('title', 'ASC')->where('status', 1)->get();
-    \Log::info(json_encode($services));
     return $services;
 
 }
